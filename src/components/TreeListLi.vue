cebc6489058c245ea03f1e139f0c027978dd8c00
liugoldent <liugoldent@gmail.com>
2024-03-11 11:19:46 +0800

    feat: 新增highLight

--- src/components/TreeListLi.vue
+++ src/components/TreeListLi.vue
@@ -1,8 +1,14 @@
 <template>
   <ul>
     <li v-for="item in data" :key="item.key">
-      <span @click="toggleTree(item)">{{ item.text }}</span>
-      <tree-list-li :tree-data="item.children" v-if="item.children && item.isOpen" />
+      <span @click="toggleTree(item)" :class="{ [$style.highLight]: item.highlight }"
+        >{{ item.text }}</span
+      >
+      <tree-list-li
+        :tree-data="item.children"
+        v-if="item.children && item.isOpen"
+        @cancelFatherHightLight="cancelFatherHightLight(item)"
+      />
     </li>
   </ul>
 </template>
@@ -16,25 +22,40 @@ export default {
       type: Array
     }
   },
-  setup(props) {
+  setup(props, { emit }) {
     const cloneTreeData = ref([])
     cloneTreeData.value = JSON.parse(JSON.stringify(props.treeData))
     const toggleTree = (item) => {
-      if (item.children) {
-        if (!item.isOpen) {
-          // 如果點擊的是已經開啟的項目，則直接關閉
-          cloneTreeData.value.forEach((i) => {
-            if (i !== item && i.isOpen) {
-              i.isOpen = false
-            }
-          })
-        }
-        item.isOpen = !item.isOpen // 切換 isOpen 屬性
+      clearHighlight(cloneTreeData.value) // 先清掉這組件所有的hightLight
+      emit('cancelFatherHightLight') // 再清掉父元素的hightLight
+      item.highlight = true // 最後此次hightLight高亮
+      if (!item.isOpen) {
+        // 如果點擊的是已經開啟的項目，則直接關閉
+        cloneTreeData.value.forEach((i) => {
+          if (i !== item && i.isOpen) {
+            i.isOpen = false
+          }
+        })
       }
+      item.isOpen = !item.isOpen // 切換 isOpen 屬性
+    }
+    // 因為toggle是針對children做切換與hightLight，所以也要emit上去讓父資料不要有hightLight
+    const cancelFatherHightLight = function (item) {
+      item.highlight = false
+    }
+    // 將本次的所有資料都先不要hightLight
+    const clearHighlight = function (items) {
+      items.forEach((item) => {
+        item.highlight = false
+        if (item.children) {
+          clearHighlight(item.children)
+        }
+      })
     }
     return {
       data: cloneTreeData,
-      toggleTree
+      toggleTree,
+      cancelFatherHightLight
     }
   }
 }
@@ -47,9 +68,14 @@ ul {
 }
 li {
   margin-bottom: 10px;
+  color: white;
 }
 span {
   display: inline-block;
   margin-bottom: 10px;
+}
+// 點選到的項目，要highLight
+.highLight {
+  color: yellow;
 }
 </style>
